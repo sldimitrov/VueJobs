4b4b866006bcb97998b58627d057ae75e8d4008f
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-09-05 22:48:26 +0300

    Composition API and Reactive values

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const name = ref('John Doe')
+  const status = ref('inactive')
+  const tasks = ref(['Task 1', 'Task 2', 'Task 3'])
+
+  const toggleStatus = () => {
+    if (status.value === 'active') {
+          status.value = 'pending';
+        } else if (status.value === 'pending') {
+          status.value = 'inactive';
+        } else {
+          status.value = 'active';
+        }
+  }
+</script>
+
+<template>
+  <h1>Vue, {{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else=>User is inactive</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" key="task">{{task}}</li>
+  </ul>
+  <br />
+  <button @click="toggleStatus">Change status</button>
+</template>
