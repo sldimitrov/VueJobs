ba40c24392b2947d23996d6cd9daf1266a87881c
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-09-05 23:27:01 +0300

    Fetch API todos and spill them in the list of items

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,44 @@
 <script setup>
-  import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
-  const name = ref('John Doe')
-  const status = ref('inactive')
-  const tasks = ref(['Init task', 'Task 2', 'Task 3'])
-  const newTask = ref('')
+const name = ref('John Doe')
+const status = ref('inactive')
+const tasks = ref(['Init task', 'Task 2', 'Task 3'])
+const newTask = ref('')
 
-  const toggleStatus = () => {
-    if (status.value === 'active') {
-          status.value = 'pending';
-        } else if (status.value === 'pending') {
-          status.value = 'inactive';
-        } else {
-          status.value = 'active';
-        }
+const toggleStatus = () => {
+  if (status.value === 'active') {
+        status.value = 'pending';
+      } else if (status.value === 'pending') {
+        status.value = 'inactive';
+      } else {
+        status.value = 'active';
+      }
+}
+
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value)
+    newTask.value = ''
   }
+}
 
-  const addTask = () => {
-    if (newTask.value.trim() !== '') {
-      tasks.value.push(newTask.value)
-      newTask.value = ''
-    }
+const popTask = () => {
+  if (tasks.value.length > 1) {
+    tasks.value.pop()
+  } 
+
+}
+
+onMounted(async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data = await response.json()
+    tasks.value = data.map((task) => task.title)
+  } catch (error) {
+    console.log('Error fetching tasks', error)
   }
-
-  const popTask = () => {
-    if (tasks.value.length > 1) {
-      tasks.value.pop()
-    } else {
-
-    }
-  }
-
+})
 </script>
 
 <template>
@@ -47,7 +55,11 @@
 
   <h3>List of Tasks:</h3>
   <ul>
-    <li v-for="task in tasks" key="task">{{task}}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>
+        {{ task }}
+      </span>
+    </li>
   </ul>
   <button @click="popTask">Pop the last task</button>
   <br />
