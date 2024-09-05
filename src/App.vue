053ded2cb9d8c7c9863d6880c1f01f39d793f54e
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-09-05 23:02:27 +0300

    Implement form that add newTasks to a list

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
   const name = ref('John Doe')
   const status = ref('inactive')
   const tasks = ref(['Task 1', 'Task 2', 'Task 3'])
+  const newTask = ref('')
 
   const toggleStatus = () => {
     if (status.value === 'active') {
@@ -14,6 +15,13 @@
           status.value = 'active';
         }
   }
+
+  const addTask = () => {
+    if (newTask.value.trim() !== '') {
+      tasks.value.push(newTask.value)
+      newTask.value = ''
+    }
+  }
 </script>
 
 <template>
@@ -21,6 +29,12 @@
   <p v-if="status === 'active'">User is active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else=>User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="task">Add task</label>
+    <input type="text" id="task" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
 
   <h3>Tasks</h3>
   <ul>
