a37df64b4a58340d8f91c5b31a4ef1f89f93edc3
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-09-06 18:37:27 +0300

    map a json data within a list

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
   import NavBar from './components/NavBar.vue';
   import Hero from './components/Hero.vue';
+  import HomeCards from './components/HomeCards.vue';
+  import JobListings from './components/JobListings.vue';
 </script>
 
 <template>
   <NavBar />
-  <Hero title="Become the best Vue Dev" subtitle="Best Crash Course" />
-  <h1>Vue Jobs</h1>
+  <Hero />
+  <HomeCards />
+  <JobListings />
 </template>
---
+++ src/components/JobListings.vue
@@ -0,0 +1,12 @@
+<script setup>
+import jsonData from '@/jobs.json'
+import { ref} from 'vue'
+
+const jobs = ref(jsonData)
+</script>
+
+<template>
+  <div v-for="job in jobs" :key="job.id">
+    {{ job.title }}
+  </div>
+</template>
